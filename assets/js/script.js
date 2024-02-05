bcad327e05f3c6e128c8b9df057db999e55bb155
Whitley Horn <whitleyblaine@protonmail.com>
2024-02-05 08:42:46 -0500

    Replace header with original design

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,23 +57,20 @@ addEventOnElem(navLinks, "click", closeNavbar);
 
 
 /**
- * header & back top btn active when scroll down to 100px
+ * back top btn active when scroll down to 100px
  */
 
-const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-const headerActive = function () {
+const backTopActive = function () {
   if (window.scrollY > 100) {
-    header.classList.add("active");
     backTopBtn.classList.add("active");
   } else {
-    header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
 }
 
-addEventOnElem(window, "scroll", headerActive);
+addEventOnElem(window, "scroll", backTopActive);
 
 
 
