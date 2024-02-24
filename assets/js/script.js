00d32d2171c334948699a08d43502a467b25ef33
Whitley Horn <whitleyblaine@protonmail.com>
2024-02-24 16:49:02 -0500

    Make footer black, add category filter back

--- assets/js/script.js
+++ assets/js/script.js
@@ -282,3 +282,35 @@ const spanishOptions = {
 
 var englishSlider = createSlider('slider-english', englishOptions);
 var spanishSlider = createSlider('slider-spanish', spanishOptions);
+ 
+/**
+ * filter function
+ */
+
+const filterBtns = document.querySelectorAll("[data-filter-btn]");
+const filterItems = document.querySelectorAll("[data-filter]");
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedFilterBtn = this;
+
+  for (let i = 0; i < filterItems.length; i++) {
+    if (this.dataset.filterBtn === filterItems[i].dataset.filter ||
+      this.dataset.filterBtn === "all") {
+
+      filterItems[i].style.display = "block";
+      filterItems[i].classList.add("active");
+
+    } else {
+
+      filterItems[i].style.display = "none";
+      filterItems[i].classList.remove("active");
+
+    }
+  }
+}
+
+addEventOnElem(filterBtns, "click", filter);
