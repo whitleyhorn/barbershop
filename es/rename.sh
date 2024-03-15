5df84131b7bb5d1f627e749998ee92e60938cec3
Whitley Horn <whitleyblaine@protonmail.com>
2024-03-14 20:31:37 -0400

    Two separate subdirectories, es and en

---
+++ es/rename.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Navegar al directorio de tus imágenes si es necesario
+# cd /ruta/a/tus/imagenes
+
+# Renombrar archivos JPG
+mv "images/gallery-1.jpg" "images/barbero-corte-pelo-berkeley-nj.jpg"
+mv "images/gallery-2.jpg" "images/peluquero-estilizando-pelo-berkeley-nj.jpg"
+mv "images/gallery-3.jpg" "images/vista-frente-barberia-berkeley.jpg"
+mv "images/gallery-4.jpg" "images/cliente-corte-nuevo-berkeley.jpg"
+mv "images/gallery-5.jpg" "images/corte-pelo-moderno-berkeley-nj.jpg"
+mv "images/gallery-6.jpg" "images/joven-corte-estiloso-berkeley.jpg"
+mv "images/gallery-7.jpg" "images/exterior-barberia-moto-berkeley.jpg"
+mv "images/gallery-8.jpg" "images/peluquero-secando-pelo-berkeley.jpg"
+mv "images/gallery-9.jpg" "images/corte-moderno-berkeley-nj.jpg"
+mv "images/gallery-11.jpg" "images/fachada-barberia-moto-berkeley.jpg"
+mv "images/gallery-12.jpg" "images/vista-trasera-corte-fresco-berkeley.jpg"
+mv "images/gallery-13.jpg" "images/peluquero-perfeccionando-corte-berkeley.jpg"
+mv "images/gallery-14.jpg" "images/interior-barberia-clientes-berkeley.jpg"
+mv "images/gallery-15.jpg" "images/dia-ocupado-barberia-berkeley.jpg"
+mv "images/gallery-16.jpg" "images/vista-lateral-corte-moderno-hombres-berkeley.jpg"
+mv "images/gallery-20.jpg" "images/cliente-disfrutando-corte-limpio-berkeley.jpg"
+
+# Renombrar archivos WEBP
+mv "images/gallery-1.webp" "images/barbero-corte-pelo-berkeley-nj.webp"
+mv "images/gallery-2.webp" "images/peluquero-estilizando-pelo-berkeley-nj.webp"
+mv "images/gallery-3.webp" "images/vista-frente-barberia-berkeley.webp"
+mv "images/gallery-4.webp" "images/cliente-corte-nuevo-berkeley.webp"
+mv "images/gallery-5.webp" "images/corte-pelo-moderno-berkeley-nj.webp"
+mv "images/gallery-6.webp" "images/joven-corte-estiloso-berkeley.webp"
+mv "images/gallery-7.webp" "images/exterior-barberia-moto-berkeley.webp"
+mv "images/gallery-8.webp" "images/peluquero-secando-pelo-berkeley.webp"
+mv "images/gallery-9.webp" "images/corte-moderno-berkeley-nj.webp"
+mv "images/gallery-11.webp" "images/fachada-barberia-moto-berkeley.webp"
+mv "images/gallery-12.webp" "images/vista-trasera-corte-fresco-berkeley.webp"
+mv "images/gallery-13.webp" "images/peluquero-perfeccionando-corte-berkeley.webp"
+mv "images/gallery-14.webp" "images/interior-barberia-clientes-berkeley.webp"
+mv "images/gallery-15.webp" "images/dia-ocupado-barberia-berkeley.webp"
+mv "images/gallery-16.webp" "images/vista-lateral-corte-moderno-hombres-berkeley.webp"
+mv "images/gallery-20.webp" "images/cliente-disfrutando-corte-limpio-berkeley.webp"
+
